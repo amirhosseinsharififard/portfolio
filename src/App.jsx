44c2e38282af1cf4837d026528b93045b49eb95d
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2024-12-11 22:58:49 +0330

    Header is done but need add sum ui's to get better and functinals

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./page/HomePage";
 import Header from "./components/Header";
-import {Container, Grid, ThemeProvider} from "@mui/material";
+import {Container,  ThemeProvider} from "@mui/material";
 import "../src/css/app.css";
 import theme from "./css/muiTheme";
+import {ThemeContext} from "@emotion/react";
+import {useState} from "react";
+
+
+
+
+
+
 function App() {
+  const [themePage, setThemePage] = useState("light");
+
+  const toggleTheme = () => {
+    setThemePage((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div>
+    <ThemeContext.Provider value={{themePage,toggleTheme}}>
       <ThemeProvider theme={theme}>
-        <Container
-        >
+        <Container>
           <Header />
           <Routes>
             <Route path='/' element={<HomePage />} />
           </Routes>
         </Container>
       </ThemeProvider>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
